ce9a826a9098fdf364a00664a0f7945f431fa78a
sutikovi-gor <sutikovi@gmail.com>
2022-04-10 20:08:43 +0300

    proba nout no change

---
+++ newsPortalV2/view/layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>NEWSPORTAL</title>
+        <link rel="stylesheet" href="
+        https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        intergrity=
+        "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+        <link rel="stylesheet" type ="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+    <meta charset="utf-8">
+
+</head>
+<body>
+    <bode>
+    
